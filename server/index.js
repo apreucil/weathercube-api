9c30623000e47ee71594fc146ffad7300c31f8b3
Matt Rota <matt.rota@opensponsorship.com>
2023-01-30 20:39:17 -0500

    disable static

--- server/index.js
+++ server/index.js
@@ -4,14 +4,15 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "../dist")));
+// app.use(express.static(path.join(__dirname, "../dist")));
 
 // use a router for all api endpoints at this path
 app.use("/api", require("./routes"));
 
 // all URLs not matching a defined route will be relegated to the front-end
 app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
